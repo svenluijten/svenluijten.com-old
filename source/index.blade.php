97a7e6a3290a2436bcfc5d432560f363dde15759
Sven Luijten <svenluijten@users.noreply.github.com>
2018-09-27 20:11:01 +0200

    content on homepage

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,17 @@
 @extends('_layouts.master')
 
 @section('body')
-<h1>Hello world!</h1>
+    <h1>Introduction</h1>
+    <p>
+        My name's Sven Luijten, and I'm a backend developer from a small town in the south of The Netherlands.
+    </p>
+
+    <h2>My Projects</h2>
+    <p>
+        I've been active in the Laravel community developing packages aimed at improving the developer experience.
+        One of my packages you may have come across is <a href="//github.com/svenluijten/artisan-view">Artisan View</a>,
+        which adds a simple <code>make:view</code> command to your Laravel application. Feel free to poke around on
+        <a href="https://github.com/svenluijten">my GitHub</a> to see some of the other projects I've worked on and
+        contributed to.
+    </p>
 @endsection
