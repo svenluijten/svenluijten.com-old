a9fdc57c82fbacb79144b7f96ed094957540a9d2
Sven Luijten <svenluijten@users.noreply.github.com>
2018-09-29 19:37:47 +0200

    prismjs + fira mono font for code blocks

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <title>Sven Luijten</title>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Sven Luijten</title>
+
         <link rel="stylesheet" href="{{ mix('/css/main.css') }}">
+        <link href="https://fonts.googleapis.com/css?family=Fira+Mono" rel="stylesheet">
     </head>
     <body class="leading-normal bg-grey-lighter antialiased subpixel-antialiased">
         <div class="xl:w-1/3 md:w-2/3 px-4 mx-auto text-lg">
             @yield('body')
         </div>
     </body>
+
+    <script src="{{ mix('/js/main.js') }}"></script>
 </html>
