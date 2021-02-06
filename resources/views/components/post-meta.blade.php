ad5c821f76d6ef8500d11fc7b27b99b06e461285
Sven Luijten <svenluijten1995@gmail.com>
2021-02-06 22:06:10 +0100

    Conditionally pluralize 'minute' to 'minutes'

--- resources/views/components/post-meta.blade.php
+++ resources/views/components/post-meta.blade.php
@@ -1,5 +1,7 @@
 <div class="text-sm text-gray-700 {{ $attributes->get('class') }} | dark:text-indigo-100">
     <span>Published on {{ $post->date()->format('F jS, Y') }}</span>
     &mdash;
-    <span class="italic">{{ $post->minutesToRead() }} minutes to read</span>
+    <span class="italic">
+        {{ $post->minutesToRead() }} {{ \Illuminate\Support\Str::plural('minute', $post->minutesToRead()) }} to read
+    </span>
 </div>
