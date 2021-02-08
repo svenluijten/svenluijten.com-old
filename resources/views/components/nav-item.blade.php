37396eaf533914d768c836e5d3d228fad7435bf5
Sven Luijten <svenluijten1995@gmail.com>
2021-02-08 22:29:23 +0100

    Center nav items in mobile view

--- resources/views/components/nav-item.blade.php
+++ resources/views/components/nav-item.blade.php
@@ -1,4 +1,4 @@
-<li class="pl-4 lg:pl-8">
+<li class="px-2 | lg:px-4">
     <a href="{{ $link }}"
        class="font-bold text-gray-600 px-4 py-2 border-b-4 border-indigo-200 | hover:text-indigo-50 hover:border-indigo-600 hover:bg-indigo-500 dark:text-indigo-100 dark:border-indigo-500"
     >
