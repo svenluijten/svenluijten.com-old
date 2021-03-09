bd2c8f6f67ca11055e97d92b0547b46e93df17a5
Sven Luijten <11269635+svenluijten@users.noreply.github.com>
2021-03-09 17:40:19 +0100

    Force HTTPS for generated URLs (#28)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /** @var \Illuminate\Contracts\Routing\UrlGenerator $url */
+        $url = $this->app->get(UrlGenerator::class);
+        $url->forceScheme('https');
     }
 }
